f9345d7935674af3a8f525fad9ffc905abf84751
CarlbertCannon <providenceneedschange@gmail.com>
2021-09-06 16:24:06 -0600

    intialized changes

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Practice_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var videoGames = new List<string>() { "Halo", "Duke Nukum", "Final Fantasy 7", "Super Mario Bros" };
+
+            var sorted = videoGames.Where(word => word.Contains(""));
+
+            //var sorted = videoGames.OrderBy(word => word.Contains(""));
+
+
+            videoGames.Sort();
+
+            foreach(var item in videoGames)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
